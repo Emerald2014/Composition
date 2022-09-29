179bd22dff6047c3f9043db34a247ac7a3274005
Kudesnik <emerald2014@mail.ru>
2022-09-29 14:34:32 +0300

    Готов UI слой

--- app/src/main/java/ru/kudesnik/composition/ui/MainActivity.kt
+++ app/src/main/java/ru/kudesnik/composition/ui/MainActivity.kt
@@ -1,10 +1,11 @@
 package ru.kudesnik.composition.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import ru.kudesnik.composition.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
