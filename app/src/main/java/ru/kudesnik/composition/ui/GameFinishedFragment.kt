6e2335ae9e2f4cafab91b7f1311bcd83b38faafb
Kudesnik <emerald2014@mail.ru>
2022-09-30 16:25:21 +0300

    Закончил GameFinishedFragment

--- app/src/main/java/ru/kudesnik/composition/ui/GameFinishedFragment.kt
+++ app/src/main/java/ru/kudesnik/composition/ui/GameFinishedFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.ViewModelProvider
+import ru.kudesnik.composition.R
 import ru.kudesnik.composition.databinding.FragmentGameFinishedBinding
 import ru.kudesnik.composition.domain.entity.GameResult
 
@@ -43,12 +45,52 @@ class GameFinishedFragment : Fragment() {
         binding.buttonRetry.setOnClickListener {
             retryGame()
         }
+        bindViews()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun bindViews() {
+        with(binding) {
+            emojiResult.setImageResource(getSmileResId())
+            tvRequiredAnswers.text = context?.resources?.let {
+                String.format(
+                    it.getString(R.string.required_score),
+                    gameResult.gameSettings.minCountOfRightAnswers
+                )
+            }
+            tvScoreAnswers.text = String.format(
+                getString(R.string.score_answers),
+                gameResult.countOfRightAnswers
+            )
+            tvRequiredPercentage.text = String.format(
+                getString(R.string.required_percentage),
+                gameResult.gameSettings.minPercentOfRightAnswers
+            )
+            tvScorePercentage.text = String.format(
+                getString(R.string.score_percentage),
+                getPercentOfRightAnswers()
+            )
+        }
+    }
+
+    private fun getSmileResId(): Int {
+        return if (gameResult.winner) {
+            R.drawable.ic_smile
+        } else R.drawable.ic_sad
+    }
+
+    private fun getPercentOfRightAnswers() = with(gameResult) {
+        if (countOfQuestions == 0) {
+            0
+        } else {
+            ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+        }
+    }
+
 
     private fun parseArgs() {
         requireArguments().getParcelable<GameResult>(KEY_GAME_RESULT)?.let {
